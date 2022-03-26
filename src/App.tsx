738dc7505527ba1f9a708542318319040283e0a4
Daniel <danieruone@gmail.com>
2022-03-26 18:44:28 -0500

    Add task component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import './App.css';
 
+// components
+import { TaskComponent } from './components/TaskComponent';
+
 function App() {
+  const allowDrop = (ev: any) => {
+    ev.preventDefault();
+  };
+
+  const drop = (ev: any) => {
+    ev.preventDefault();
+    console.log(ev);
+    var data = ev.dataTransfer.getData('text');
+    ev.target.appendChild(document.getElementById(data));
+  };
+
   return (
     <div className='container'>
       <h2>Trello App</h2>
       <div className='grid'>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div
+          className='grid-sections'
+          onDrop={(e) => drop(e)}
+          onDragOver={(e) => allowDrop(e)}
+        >
+          <TaskComponent />
+        </div>
+        <div
+          className='grid-sections'
+          onDrop={(e) => drop(e)}
+          onDragOver={(e) => allowDrop(e)}
+        ></div>
+        <div
+          className='grid-sections'
+          onDrop={(e) => drop(e)}
+          onDragOver={(e) => allowDrop(e)}
+        ></div>
       </div>
     </div>
   );
---
+++ src/components/TaskComponent/index.tsx
@@ -0,0 +1,5 @@
+import './index.css';
+
+export const TaskComponent = () => {
+  return <div className='task-container'>index</div>;
+};
